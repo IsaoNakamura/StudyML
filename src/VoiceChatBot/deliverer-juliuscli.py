d2a20ca07701bf71dcb3693b0f068a3ed3821437
IsaoNakamura <gariox7@gmail.com>
2021-02-25 19:42:24 +0900

    Working.

--- src/VoiceChatBot/deliverer-juliuscli.py
+++ src/VoiceChatBot/deliverer-juliuscli.py
@@ -15,87 +15,102 @@ import socket
 from packages.util_socket import util_socket
 from packages.util_signal import util_signal
 
-juliusserver_host = 'localhost'
-juliusserver_port = 10500
-
-talker_host =  'localhost'
-talker_port = 5533
-
-def main():
-    result = -1
-    juliusserversock = None
-    talkersock = None
-
-    try:
+class DelivererJuliusCli:
+    def __init__(
+        self
+        , _juliusmodule_host:str='localhost'
+        , _juliusmodule_port:int=10500
+        , _talker_host:str='localhost'
+        , _talker_port:int=5533
+    ):
+        """コンストラクタ"""
+        print("init. : {}".format(os.path.basename(__file__)))
         util_signal.set_killtrap_handler()
-
-        juliusserversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if( juliusserversock is None ):
-            return result
-
-        talkersock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if( talkersock is None ):
-            return result
-
-        if( util_socket.connectSocketRetry(juliusserversock, juliusserver_host, juliusserver_port,100) != 0):
-            return result
-        if( util_socket.connectSocketRetry(talkersock, talker_host, talker_host) != 0):
-            return result
-
-        buffer = ''
-        while True:
-            rcvmsg = str(juliusserversock.recv(1024).decode('utf-8'))      
-            if len(rcvmsg)>0:
-                buffer+=rcvmsg
-                if '</RECOGOUT>\n.' in buffer:
-                    recog_text = ''
-                    #for line in buffer.split('\n'):
-                    #    index = line.find('WORD="')
-                    #    if index != -1:
-                    #        line = line[index+6:line.find('"',index+6)]
-                    #        recog_text = recog_text + line
-                    #print("send to talker for recognition. text={}".format(recog_text))
-                    #talkersock.send(recog_text.encode("UTF-8"))
-                    
-                    for line in buffer.split('\n'):
-                        index = line.find('CLASSID="')
-                        if index != -1:
-                            line = line[index+9:line.find('"',index+9)]
-                            if '&lt;s&gt;' not in line and '&lt;/s&gt;' not in line :
-                                recog_text = recog_text + line + ' '
-                    print("send to talker for recognition. text={}".format(recog_text))
-                    talkersock.send(recog_text.encode("UTF-8"))
-                    
-                    buffer=''
-                elif '<RECOGFAIL/>\n.' in buffer:
-                    print("send to talker for failed.")
-                    talkersock.send("/recogfail".encode("UTF-8"))
-                    buffer=''
-                elif '<REJECTED REASON=' in buffer:
-                    print("send to talker for rejected.")
-                    talkersock.send("/recogfail".encode("UTF-8"))
-                    buffer=''
-    except KeyboardInterrupt:
-        # Ctrl+Cで止められるのが前提
-        # print("{} : except KeyboardInterrupt.".format(os.path.basename(__file__)))
-        result = 0
-    except Exception as e:
-        print(e)
-    finally:
+        self.juliusmodule_host = _juliusmodule_host
+        self.juliusmodule_port = _juliusmodule_port
+        self.talker_host =  _talker_host
+        self.talker_port = _talker_port
+        self.juliusmodule_sock = None
+        self.talker_sock = None
+    
+    def __del__(self):
+        """デストラクタ"""
+        print("del. : {}".format(os.path.basename(__file__)))
+        
         util_signal.set_killtrap_finallybeg()
-        # print("{} : finally beg.".format(os.path.basename(__file__)))
-        if(talkersock is not None):
-            talkersock.close()
-        if(juliusserversock is not None):
+        print("finally beg. : {}".format(os.path.basename(__file__)))
+
+        if(self.talker_sock is not None):
+            self.talker_sock.close()
+        if(self.juliusmodule_sock is not None):
             #juliusserversock.send("DIE".encode('utf-8'))
-            juliusserversock.close()
-        if(result!=0):
-            pass
-        # print("{} : finally end.".format(os.path.basename(__file__)))
+            self.juliusmodule_sock.close()
+
+        print("finally end. : {}".format(os.path.basename(__file__)))
         util_signal.set_killtrap_finallyend()
-    return result
+
+    def run_main(self):
+        result = -1
+        try:
+            self.juliusmodule_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if( self.juliusmodule_sock is None ):
+                return result
+
+            self.talker_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if( self.talker_sock is None ):
+                return result
+
+            if( util_socket.connectSocketRetry(self.juliusmodule_sock, self.juliusmodule_host, self.juliusmodule_port, 1) != 0):
+                return result
+            if( util_socket.connectSocketRetry(self.talker_sock, self.talker_host, self.talker_port) != 0):
+                return result
+
+            buffer = ''
+            while True:
+                rcvmsg = str(self.juliusmodule_sock.recv(1024).decode('utf-8'))      
+                if len(rcvmsg)>0:
+                    buffer+=rcvmsg
+                    if '</RECOGOUT>\n.' in buffer:
+                        recog_text = ''
+                        #for line in buffer.split('\n'):
+                        #    index = line.find('WORD="')
+                        #    if index != -1:
+                        #        line = line[index+6:line.find('"',index+6)]
+                        #        recog_text = recog_text + line
+                        #print("send to talker for recognition. text={}".format(recog_text))
+                        #talkersock.send(recog_text.encode("UTF-8"))
+                        
+                        for line in buffer.split('\n'):
+                            index = line.find('CLASSID="')
+                            if index != -1:
+                                line = line[index+9:line.find('"',index+9)]
+                                if '&lt;s&gt;' not in line and '&lt;/s&gt;' not in line :
+                                    recog_text = recog_text + line + ' '
+                        print("send to talker for recognition. text={}".format(recog_text))
+                        self.talker_sock.send(recog_text.encode("UTF-8"))
+                        
+                        buffer=''
+                    elif '<RECOGFAIL/>\n.' in buffer:
+                        print("send to talker for failed.")
+                        self.talker_sock.send("/recogfail".encode("UTF-8"))
+                        buffer=''
+                    elif '<REJECTED REASON=' in buffer:
+                        print("send to talker for rejected.")
+                        self.talker_sock.send("/recogfail".encode("UTF-8"))
+                        buffer=''
+        except KeyboardInterrupt:
+            # Ctrl+Cで止められるのが前提
+            print("except KeyboardInterrupt. : {}".format(os.path.basename(__file__)))
+            result = 0
+        except Exception as e:
+            print("except Exception. :{} {}".format(os.path.basename(__file__), e))
+        finally:
+            if(result!=0):
+                pass
+        return result
+
 
 if __name__ == '__main__':
-    sys.exit(abs(main()))
+    sys.exit(abs(DelivererJuliusCli().run_main()))
 
     
--- src/packages/util_signal/util_signal.py
+++ src/packages/util_signal/util_signal.py
@@ -2,7 +2,7 @@ import sys
 import signal
 
 def killtrap_handler(signum, frame) -> None:
-    #print("trapped. signum={} frame={}".format(signum, frame))
+    print("trapped. signum={} frame={}".format(signum, frame))
     sys.exit(1)
 
 def set_killtrap_handler() -> None:
@@ -16,3 +16,30 @@ def set_killtrap_finallyend() -> None:
     signal.signal(signal.SIGTERM, signal.SIG_DFL)
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+# 自作signalユーティリティモジュールを参照
+from packages.util_signal import util_signal
+class Hoge:
+    def __init__(self):
+        util_signal.set_killtrap_handler()
+        # 初期化処理
+
+    def __del__(self):
+        util_signal.set_killtrap_finallybeg()
+        # 後始末処理
+        util_signal.set_killtrap_finallyend()
+    
+    def run_main(self):
+        result = -1
+        try:
+            # メイン処理
+            pass
+        except Exception as e:
+            print(e)
+        finally:
+            if(result!=0):
+                pass
+        return result
+
+if __name__ == '__main__':
+    # エントリポイント
+    sys.exit(Hoge().run_main())
